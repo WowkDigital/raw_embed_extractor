0de2ac3701d8384c53e54d4900f83bf47c154d89
Lena Piszczek <lena64_t@riseup.net>
2024-04-25 23:17:11 +0200

    - Heavily refactored - Code is now handling non-existent directory properly - Skipping already processed files - Removed creating dirs with suffixes - Now, the script is creating folder *aside* processed one - removed input prompt

--- raw_embed_extractor_v1.py
+++ raw_embed_extractor_v1.py
@@ -23,38 +23,40 @@ def rotate_image_based_on_exif(image):
         print(f"Error processing EXIF data: {e}")
     return image
 
-def process_image(raw_file_path, jpg_folder_path):
+def process_image(raw_file_path, output_file):
     with rawpy.imread(raw_file_path) as raw:
         embedded_image = raw.extract_thumb()
         if embedded_image.format == rawpy.ThumbFormat.JPEG:
             img = Image.open(io.BytesIO(embedded_image.data))
             img = rotate_image_based_on_exif(img)
-            final_path = os.path.join(jpg_folder_path, os.path.splitext(os.path.basename(raw_file_path))[0] + '.jpg')
-            img.save(final_path)
-            print(f"Saved and rotated JPG preview at: {final_path}")
+            img.save(output_file)
+            print(f"Saved and rotated JPG preview at: {output_file}")
         else:
             print("Embedded JPG image not found in RAW file.")
 
-def process_raw_folder(folder_path):
-    jpg_folder_path = os.path.join(folder_path, 'jpg')
-    create_folder = not os.path.exists(jpg_folder_path)
-    if not create_folder:
-        suffix = 1
-        while os.path.exists(f"{jpg_folder_path}_{suffix}"):
-            suffix += 1
-        jpg_folder_path = f"{jpg_folder_path}_{suffix}"
-        create_folder = True
-    if create_folder:
+def process_raw_folder(raws_dir):
+    jpg_folder_path = os.path.join(raws_dir, '..', 'thumbs_jpg')
+    
+    if not os.path.exists(jpg_folder_path):
         os.makedirs(jpg_folder_path)
     
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(raws_dir):
         if filename.lower().endswith(('.arw', '.cr2')):
-            process_image(os.path.join(folder_path, filename), jpg_folder_path)
+            output_file = os.path.join(jpg_folder_path, os.path.splitext(os.path.basename(filename))[0] + '.jpg')
+            if os.path.exists(output_file):
+                print(f"Skipping {output_file} (it already exists)")
+            else:
+                process_image(os.path.join(raws_dir, filename), output_file)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: script.py <folder_path>")
+        pyfile = os.path.basename(__file__)
+        print(f"Usage: {pyfile} <dir with raws>")
         sys.exit(1)
-    folder_path = sys.argv[1]
-    process_raw_folder(folder_path)
-    input("Press enter to continue...")
+    raws_dir = sys.argv[1]
+    if os.path.isdir(raws_dir):
+        process_raw_folder(raws_dir)
+    else:
+        print("Given path does not exist or is not a directory")
+        sys.exit(2)
+    # input("Press enter to continue...")
